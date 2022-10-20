1436c965b598dbf781a33934bec49a362429bde8
Anthony77151 <72447583+Anthony77151@users.noreply.github.com>
2022-10-20 15:49:16 +0200

    upload file on API

---
+++ back/controllers/upload.js
@@ -0,0 +1,32 @@
+const UserModel = require('../models/User');
+const { uploadErrors } = require('../utils/errors');
+const fs = require('fs');
+const { promisify } = require('util');
+const pipeline = promisify(require('stream').pipeline);
+
+module.exports.uploadProfil = async (req, res) => {
+    try {
+        // vérification du format de l'image
+        if (
+            req.file.detectedMimeType != 'image/jpg' &&
+            req.file.detectedMimeType != 'image/jpeg' &&
+            req.file.detectedMimeType != 'image/png' &&
+            req.file.detectedMimeType != 'image/webp'
+        )
+            throw Error('invalid file');
+        if (req.file.size > 500000)
+            throw Error('max size');
+    } catch (error) {
+        const errors = uploadErrors(error);
+        return res.status(201).json({ errors });
+    }
+
+    const fileName = req.body.name + ".webp";
+
+    await pipeline(
+        req.file.stream,
+        fs.createWriteStream(
+            `${__dirname}/../../front/public/images/profil/${fileName}`
+        ),
+    );
+}
